c686c4ea79afddee05f4ea16e9228018655b447a
Munasinghe MART <ravinduthathsara38@gmail.com>
2026-03-20 12:41:31 +0530

    Revert "Fixed cors error"

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 
 export const configureCors = () => {
   return cors({
-    origin: true,
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
     credentials: true,
   });
 };
