5eaa5988e725d7bbc434817f1b13f3501876e0e6
Wannige D D <dilshandilushawannige@gmail.com>
2026-03-20 13:11:08 +0530

    Revert "Fix/uidashboard dilshan"

--- backend/src/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-// Wraps an async route handler and forwards any rejected promise to Express's next()
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-export default asyncHandler;
